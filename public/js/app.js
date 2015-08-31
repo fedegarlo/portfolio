f72438d57f012c2a9a33ba401a9bcad1b1d5f36f
Fede García <f.garcia-lorca.contractor@bbva.com>
2015-08-31 13:31:36 +0200

    [FIX] Faltaban unas librerías

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+(function() {
+  var myApp = angular.module("myApp", ['ngRoute', 'ngL20n']);
+
+myApp.config(function ($routeProvider, $httpProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'html/home.html',
+            controller: 'MainController'
+        }).otherwise({
+        redirectTo : '/home'         
+        }
+    )
+    }).run(['$rootScope', 'documentL10n', 'l20n', function ($rootScope, documentL10n, l20n) {
+            $rootScope.l20nId = 'objectsWithCount';
+            $rootScope.data = {
+                objectsNum: 102,
+                testNumber: 0,
+            };
+
+            function setObjectsNum(number) {
+                l20n.updateData({
+                    objectsNum: number,
+                });
+            }
+
+            $rootScope.$watch('data.objectsNum', function (newValue) {
+                setObjectsNum(parseInt(newValue, 10) || 0);
+            });
+
+            documentL10n.ready(function () {
+                setObjectsNum(parseInt($rootScope.data.objectsNum, 10) || 0);
+            });
+        }]);
+
+}());
---
+++ public/js/controllers/MainController.js
@@ -0,0 +1,67 @@
+(function(){
+  var app=angular.module("myApp");
+
+var MainCtrl = function($scope){
+	$scope.works = [
+		{
+			title : 'BBVA',
+			new : true,
+			img : 'bbva.png',
+			id : 'helping',
+			url : 'http://www.bbva.es'
+		},{
+			title : 'Apple Watch Catalogue',
+			new : true,
+			img : 'watches.png',
+			id : 'watch_description',
+			url : '/sites/macfools/watch/index.html'
+		},{
+			title : 'Ana y Fede 2014',
+			new : false,
+			img : 'fedeyana.png',
+			id : 'wedding_description',
+			url : '/sites/anayfede/index.html'
+		},{
+			title : 'Corpo Derma',
+			new : false,
+			img : 'corpoderma_logo.png',
+			id : 'corpoderma_description',
+			url : '/sites/corpoderma/index.html'
+		},{
+			title : 'Money Labels',
+			new : false,
+			img : 'moneylabels_logo.png',
+			id : 'moneylabels_description',
+			url : 'https://marketplace.firefox.com/app/moneylabels/'
+		},{
+			title : 'Abece Logopedas',
+			new : false,
+			img : 'abece_logo.png',
+			id : 'abece_description',
+			url : '/sites/abece/index.html'
+		},{
+			title : 'Bevip',
+			new : false,
+			img : 'bevip_logo.png',
+			id : 'bevip_description',
+			url : '/sites/bevip/index.html'
+		},{
+			title : 'Agrupación Marjales',
+			new : false,
+			img : 'marjales_logo.png',
+			id : 'marjales_description',
+			url : '/sites/marjales/index.html'
+		},{
+			title : 'Norestenet',
+			new : false,
+			img : 'norestenet_logo.png',
+			id : 'noreste_description',
+			url : '/sites/norestenet/index.html'
+		}
+	];
+
+};
+
+app.controller("MainController",["$scope",MainCtrl]);
+
+}());
