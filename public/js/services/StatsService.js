d8126730f686b3f9817d2752ad64e20889ea54cf
Federico <fedegarcia@icloud.com>
2017-09-18 08:54:21 +0200

    Cambio de endpoint de stats

--- public/js/services/StatsService.js
+++ public/js/services/StatsService.js
@@ -4,7 +4,7 @@ angular.module("myApp").factory('statsService', ['$http', function($http){
   var o = {
   	stats : []
   },
-  url = 'http://salva-fedegarloapps.rhcloud.com/';
+  url = 'http://gitlab-watcher-salva-watcher.7e14.starter-us-west-2.openshiftapps.com/';
   o.getStats = function() {
     return $http.get(url + 'stats').then(function(res){
 			angular.copy(res.data, o.stats);
