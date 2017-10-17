197f4267c9ce499b94b128ea0db27a5dc559f933
Federico <fedegarcia@icloud.com>
2017-10-17 09:40:52 +0200

    Fix

--- public/js/services/StatsService.js
+++ public/js/services/StatsService.js
@@ -4,7 +4,7 @@ angular.module("myApp").factory('statsService', ['$http', function($http){
   var o = {
   	stats : []
   },
-  url = 'http://salva-fedegarloapps.rhcloud.com/';
+  url = 'http://salva-api-salva.a3c1.starter-us-west-1.openshiftapps.com/';
   o.getStats = function() {
     return $http.get(url + 'stats').then(function(res){
 			angular.copy(res.data, o.stats);
